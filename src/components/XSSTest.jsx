c3129c49d9852c37f5760aa70e8409d3cbede905
NathanJAOMAMY <nathanjaomamy@gmail.com>
2026-03-17 17:38:21 +0300

    faille XSS

--- src/components/XSSTest.jsx
+++ src/components/XSSTest.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import { useSearchParams } from 'react-router-dom';
 
 const XSSTest = () => {
-  const [searchParams] = useSearchParams();
-  // Si aucun paramètre 'msg', on utilise un payload XSS par défaut
-  const msg = searchParams.get('msg') || '<img src=x onerror=alert(1)>';
   return (
     <div style={{ padding: '20px' }}>
       <h2>Test XSS (vulnérable)</h2>
-      <div dangerouslySetInnerHTML={{ __html: msg }} />
+      <div dangerouslySetInnerHTML={{ __html: '<img src=x onerror=alert(1)>' }} />
     </div>
   );
 };
